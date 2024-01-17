a1c0c8d612cd224bdff544b97e898146d47961b3
ginItchimaru <linoilday@gmail.com>
2024-01-17 13:10:13 +0100

    added animation class

---
+++ animation.cpp
@@ -0,0 +1,32 @@
+#include "animation.h"
+#include<iostream>
+
+void Animation::initVariables() {
+
+
+
+}
+
+Animation::Animation() {
+
+	initVariables();
+
+}
+
+Animation::~Animation() {
+
+
+
+}
+
+void Animation::update() {
+
+
+
+}
+
+void Animation::render() {
+
+
+
+}
---
+++ animation.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include"target.h"
+
+
+class Animation {
+
+private:
+
+	void initVariables();
+
+public:
+
+	Animation();
+	~Animation();
+
+	void update();
+	void render();
+
+};
+
